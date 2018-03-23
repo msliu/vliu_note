f062dc522194ebc6b0d16f2c7c8fdeaac7d09a58
msliu <vincent.liu1013@gmail.com>
2018-03-23 08:27:17 +0800

    20180323-01-update

---
+++ vliu.cpp
@@ -0,0 +1,17 @@
+@@@@@@@@@@@@@@@@@ 20180323
+
+1. git command to init a repo
+   echo "# Golang" >> README.md
+   git init
+   git add README.md
+   git commit -m "first commit"
+   git remote add origin git@github.com:msliu/Golang.git
+   git push -u origin master
+
+2. Links to install Glang with Homebrew
+   http://sourabhbajaj.com/mac-setup/Go/README.html
+
+3. Launch Sublime Text 2 from the Mac OS X Terminal
+   https://gist.github.com/artero/1236170
+
+   Cmd: ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/sublime
